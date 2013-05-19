65e3b326fef831874c3613ec4f42a7b28d0147a3
Lukasz Pielaszek <loky91@gmail.com>
2013-05-19 17:18:27 +0200

    Dodano deklarację klasy odpowiedzialnej za wnioskowanie w pakiecie kontrolera

---
+++ src/pl/edu/pw/elka/maszyna/kontroler/KontrolerWnioskowania.java
@@ -0,0 +1,23 @@
+package pl.edu.pw.elka.maszyna.kontroler;
+
+import java.util.List;
+
+import pl.edu.pw.elka.maszyna.entity.Klauzula;
+
+/**
+ * @author lpielasz
+ *
+ */
+public class KontrolerWnioskowania {
+	
+	/**
+	 * @param listaKlauzul
+	 * @return wynik wnioskowania
+	 * Funkcja swoim działaniem generuje nowe Klauzule
+	 * i umieszcza je w {@code listaKlauzul}. 
+	 */
+	public static boolean wnioskuj(List<Klauzula> listaKlauzul){
+		//TODO
+		return false;
+	}
+}
