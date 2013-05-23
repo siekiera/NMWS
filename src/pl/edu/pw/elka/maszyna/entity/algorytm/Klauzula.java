670ece9333d59f95485d2824fea1339918b2d272
Lukasz Pielaszek <loky91@gmail.com>
2013-05-23 17:58:03 +0200

    Dodano funkcje umozliwiajace sprawdzenie rezolucjowalnosci w Klauzula i Literal

--- src/pl/edu/pw/elka/maszyna/entity/algorytm/Klauzula.java
+++ src/pl/edu/pw/elka/maszyna/entity/algorytm/Klauzula.java
@@ -1,5 +1,6 @@
 package pl.edu.pw.elka.maszyna.entity.algorytm;
 
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -9,6 +10,30 @@ import java.util.Set;
 public class Klauzula 
 {
 	public Set<Literal> literaly;
+	
+	
+	
+	/**
+	 * Sprawdza czy na dwoch klauzulach mozna przeprowadzic rezolucje.
+	 * @param inna
+	 * @return
+	 */
+	public boolean sprawdzRezolucjowalnosc(Klauzula inna){
+		
+		Iterator gorna = literaly.iterator();
+		Iterator dolna = inna.literaly.iterator();
+		
+		while(gorna.hasNext()){
+			Literal literalGornej = (Literal)gorna.next();
+			while(dolna.hasNext()){
+				if(dolna.next().equals(literalGornej)) return true;
+			}
+			dolna = inna.literaly.iterator();
+		}
+		
+		return false;
+	}
+	
 }
 
 
