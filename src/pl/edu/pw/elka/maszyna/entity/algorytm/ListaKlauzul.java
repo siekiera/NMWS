d4ab6229988cecb5c5b094a9d953e7e88d308feb
Lukasz Pielaszek <loky91@gmail.com>
2013-05-25 18:00:21 +0200

    Dodano funkcje usuwajaca klauzule na pewno prawdziwe

--- src/pl/edu/pw/elka/maszyna/entity/algorytm/ListaKlauzul.java
+++ src/pl/edu/pw/elka/maszyna/entity/algorytm/ListaKlauzul.java
@@ -1,6 +1,7 @@
 package pl.edu.pw.elka.maszyna.entity.algorytm;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ListaKlauzul {
@@ -14,6 +15,32 @@ public class ListaKlauzul {
     public List<Klauzula> getKlauzule() {
 		return klauzule;
 	}
+    
+    
+    /**
+     * Usowa klauzule, które maj¹ w sobie literal i zaprzeczony ten sam literal. To oznacza prawdê i taka klauzula nic nie wnosi.
+     */
+    public void usunPrawdziweKlauzule(){
+    	
+    	Iterator klauzuleIterator = klauzule.iterator();
+    	
+    	while(klauzuleIterator.hasNext()){
+    		Klauzula element = (Klauzula)klauzuleIterator.next();
+    		
+    		outerloop:
+    		for(Object zew: element.literaly){
+    			for(Object wew: element.literaly){
+    				Literal lLiteral = (Literal)zew;
+    				Literal pLiteral = (Literal)wew;
+    				if(lLiteral.rezolucjowalny(pLiteral)){
+    					klauzule.remove(element);
+    					klauzuleIterator = klauzule.iterator();
+    					break outerloop;
+    				}
+    			}
+    		}
+    	}
+    }
     
     @Override
     public String toString() {
